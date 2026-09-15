e896a307ba70d4b78307471f2c4a0a871bcf8a57
BerndA <BerndA@users.noreply.github.com>
2026-09-15 13:21:48 +0200

    feat(starship): add starship

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -59,6 +59,7 @@
     pkgs.skopeo
     pkgs.ssss
     pkgs.sshpass
+    pkgs.starship
     pkgs.tig
     pkgs.tio
     pkgs.tmux
@@ -210,5 +211,20 @@
   programs.navi = {
     enable = true;
     enableZshIntegration = true;
+  };
+
+  programs.starship = {
+    enable = true;
+    # Configuration written to ~/.config/starship.toml
+    settings = {
+      # add_newline = false;
+
+      # character = {
+      #   success_symbol = "[➜](bold green)";
+      #   error_symbol = "[➜](bold red)";
+      # };
+
+      # package.disabled = true;
+    };
   };
 }
